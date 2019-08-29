f58e443d3c28ec13d25d3469aee5a79e336c16e9
Hudson643 <54511879+Hudson643@users.noreply.github.com>
2019-08-29 12:57:23 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Hudson Hill & Victoria Nelson
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hudson & Victoria\n");
+
+  return 0;
+}
